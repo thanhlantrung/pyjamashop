cf5393c19afc160add4c3be39734b10aa0e6f0c8
Thanh <thanhlan.trung@gmail.com>
2016-05-13 11:44:06 +0700

    added links

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/about'
+  get 'static_pages/about', to: 'static_pages#about'
 
-  get 'static_pages/contact'
-
-  get 'static_pages/about'
-
-  get 'static_pages/contact'
+  get 'static_pages/contact', to: 'static_pages#contact'
 
   get 'static_pages/index'
 
