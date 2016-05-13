5c46f14f2009484f7447effc07684127b5d8c79c
Thanh <thanhlan.trung@gmail.com>
2016-05-13 18:46:42 +0700

    fixed links

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'static_pages/about', to: 'static_pages#about'
+  get 'about', to: 'static_pages#about'
 
-  get 'static_pages/contact', to: 'static_pages#contact'
+  get 'contact', to: 'static_pages#contact'
 
-  get 'static_pages/index'
+  get 'index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
