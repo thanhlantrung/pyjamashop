3370d51cbc0806f1276e78fcf170c891742ca37e
Thanh <thanhlan.trung@gmail.com>
2016-05-19 12:48:51 +0700

    updated products

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160516135517) do
+ActiveRecord::Schema.define(version: 20160519054033) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20160516135517) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "color"
+    t.decimal  "price"
   end
 
 end
