4fcf3730892d35505394347ba7393862b19ecfbc
Thanh <thanhlan.trung@gmail.com>
2016-06-03 14:00:44 +0700

    updated mailer

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,10 +10,7 @@ class StaticPagesController < ApplicationController
   	@name = params[:name]
   	@email = params[:email]
   	@message =[:message]
-  	ActionMailer::Base.mail(:from => @email,
-  		:to => 'thanhlan.trung@gmail.com',
-  		:subject => "New message from #{@name}",
-  		:body => @message).deliver_now
+  	UserMailer.contact_form(@email, @name, @message).deliver_now
   end
   
 end
---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,4 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: "thanhlan.trung@gmail.com"
+  layout 'mailer'
+end
