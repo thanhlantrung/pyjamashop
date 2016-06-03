258bcc7c06c4e6b12ca1d560b959077910e508dc
Thanh <thanhlan.trung@gmail.com>
2016-06-03 13:32:54 +0700

    updated contact form

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,5 +5,15 @@ class StaticPagesController < ApplicationController
   def landing_page
   	@products = Product.all
   end
+
+  def thank_you
+  	@name = params[:name]
+  	@email = params[:email]
+  	@message =[:message]
+  	ActionMailer::Base.mail(:from => @email,
+  		:to => 'thanhlan.trung@gmail.com',
+  		:subject => "New message from #{@name}",
+  		:body => @message).deliver_now
+  end
   
 end
