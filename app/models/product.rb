9c16be45df24390ef3ff623090a2865f05f4d5db
Thanh <thanhlan.trung@gmail.com>
2016-06-20 18:36:50 +0700

    product ratings

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ActiveRecord::Base
 	has_many :comments
 end
+
+def average_rating
+	comments.average(:rating).to_f
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
+  resources :products do
+    resources :comments 
+  end
   resources :users
-  resources :products
   get 'about', to: 'static_pages#about'
 
   get 'contact', to: 'static_pages#contact'
