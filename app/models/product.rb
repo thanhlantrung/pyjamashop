0eb7acdc060f6cae8a6ecdac4f84ccb7b6fc5649
Thanh <thanhlan.trung@gmail.com>
2016-06-22 23:06:49 +0700

    updated products show

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,8 @@
 class Product < ActiveRecord::Base
 	has_many :comments
-end
 
-def average_rating
-	comments.average(:rating).to_f
-end 
+
+	def average_rating
+		comments.average(:rating).to_f
+	end 
+end
