312be931d14e2669a62699961e75cc855325b739
Thanh Trung <thanhlan.trung@gmail.com>
2016-07-04 19:42:28 +1000

    welcome email

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 	has_many :orders
+
+
+after_create :send_welcome_email
+
+private
+	def send_welcome_email
+		UserMailer.welcome(self).deliver_now
+	end
 end
