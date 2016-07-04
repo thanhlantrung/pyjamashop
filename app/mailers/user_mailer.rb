e95284598b589a27baec6d811ed5733a4c6827d8
Thanh Trung <thanhlan.trung@gmail.com>
2016-07-04 12:19:59 +1000

    added sign up email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,4 +7,10 @@ class UserMailer < ApplicationMailer
 			:to => 'thanhlan.trung@gmail.com',
 			:subject => "A new contact form message from #{name}")
 	end
+
+	def welcome_email(user)
+		@user = user
+		@url = 'http://thepyjamashop.herokuapp.com/sign_up'
+		mail(to: @user.email, subject: 'Welcome to The Pyjama Shop')
+	end
 end
