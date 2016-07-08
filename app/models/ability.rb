4c03d79bd9ace3020055ace671d752e9a838e6aa
Thanh Trung <thanhlan.trung@gmail.com>
2016-07-08 10:56:56 +1000

    ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,10 +5,10 @@ class Ability
     #user ||= User.new # guest user (not logged in)
     #can :manage, User, id: user.id
   #end
-
+end
   if user.admin?
   can :manage, User
 else
   can :manage, User, id: user.id
 end
-end
+
