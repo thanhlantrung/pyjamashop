f2303badb5960c82b4b3bce4b7d655a8af61b2cf
Thanh Trung <thanhlan.trung@gmail.com>
2016-07-08 10:51:59 +1000

    updated ability.rb

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,4 +5,10 @@ class Ability
     user ||= User.new # guest user (not logged in)
     can :manage, User, id: user.id
   end
+
+  if user.admin?
+  	can :manage, User
+  else
+  	can :manage, User, id: user.id
+  end 
 end
