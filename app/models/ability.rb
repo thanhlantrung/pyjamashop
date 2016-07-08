624f4ba498b883a0a438f95e5d2db7c8ce57c45f
Thanh Trung <thanhlan.trung@gmail.com>
2016-07-08 19:11:59 +1000

    admin and signup

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,6 +3,11 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    can :manage, User, id: user.id
+    #can :manage, User, id: user.id
+  
+  if user.admin?
+  	can :manage, User
+  else
+  	can :manage, User, id: user.id
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,10 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # inside config/production.rb
+  config.action_mailer.default_url_options = { :host => 'thepyjamashop.herokuapp.com' }
+  
+  # inside config/test.rb
+  config.action_mailer.default_url_options = {:host => "localhost:3000"}
 end
