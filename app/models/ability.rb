f93eb95e25cb69de78b77f2818ba6f5ac8a69f6e
Thanh Trung <thanhlan.trung@gmail.com>
2016-07-08 10:59:33 +1000

    ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,14 +1,8 @@
 class Ability
   include CanCan::Ability
 
-  #def initialize(user)
-    #user ||= User.new # guest user (not logged in)
-    #can :manage, User, id: user.id
-  #end
-end
-  if user.admin?
-  can :manage, User
-else
-  can :manage, User, id: user.id
-end
+  def initialize(user)
+    user ||= User.new # guest user (not logged in)
+    can :manage, User, id: user.id
+  end
 
