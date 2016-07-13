dd2f3b21d756d0fbc8b124c3f2a3660bb0e78421
Thanh Trung <thanhlan.trung@gmail.com>
2016-07-13 23:46:05 +1000

    6.3 fixed spaces in code

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe UsersController, :type => :controller do 
+	before do
+		@user =  User.create!(email: "test@email.com", password: "test123")
+	end
+
+	describe "GET #show" do
+		context "User is logged in" do
+			before do
+				sign_in @user
+			end
+			
+				it "loads correct user details" do
+					get :show, id: @user.id
+			expect(response).to have_http_status(200)
+			expect(assigns(:user)).to eq @user 
+				end
+		end
+
+		context "No user is logged in" do
+			it "redirects to login" do
+				get :show, id: @user.id
+				expect(response).to redirect_to(new_user_session_path)
+			end
+		end
+	end
+end
