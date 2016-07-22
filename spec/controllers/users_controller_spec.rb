7d611228e5d6b39e40f533e207073450d6175279
Thanh Trung <thanhlan.trung@gmail.com>
2016-07-22 16:17:28 +1000

    6.4 have updated to correct code

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,68 +1,58 @@
 require 'rails_helper'
 
-describe UsersController, :type => :controller do 
-	#before do
-		#@user =  User.create!(email: "test@email.com", password: "test123")
-		let(:user){FactoryGirl.create(:user)}
-		#@usertwo = User.create!(email: "test2@email.com", password: "test123")
-		let(:user_two){FactoryGirl.create(:user_two)}
-		
+describe UsersController, :type => :controller do
+  let(:user) { FactoryGirl.create(:user) }
+  let(:user_two) { FactoryGirl.create(:user) }
 
-	end
+  describe "GET #show" do
+    context "User is logged in" do
+      before do
+        sign_in user
+      end
 
-	describe "GET #show" do
-		
-		context "User is logged in" do
-			before do
-				sign_in user
-			end
-			
-				it "loads correct user details" do
-					get :show, id: user.id
-			expect(response).to be_success
-			expect(response).to have_http_status(200)
-			expect(assigns(:user)).to eq user 
-				end
-		end
+      it "loads correct user details" do
+        get :show, id: user.id
+        expect(response).to be_success
+        expect(response).to have_http_status(200)
+        expect(assigns(:user)).to eq user 
+      end
+    end
 
-		context "No user is logged in" do
-			it "redirects to login" do
-				get :show, id: user.id
-				expect(response).to redirect_to(root_path)
-			end
-		end
-	end
+    context "No user is logged in" do
+      it "redirects to login" do
+        get :show, id: user.id
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
 
-	describe "GET #edit" do
-		context "User is logged in and tries to edit profile" do
-			before do
-				sign_in user_two
-			end
+    context "User viewing other user profiles" do
+      before do
+        sign_in user_two
+      end
 
-			it "should redirect to the user's edit page" do
-				get :edit, id: user_two.id
-				expect(response).to have_http_status(200)
-			end
+      it "should block user and redirect to root path" do
+        get :show, id: user.id
+        expect(response).to redirect_to("/")
+      end
+    end    
+  end
 
-			it "should block other users from editing pages" do
-				get :edit, id: user.id
-				expect(response).not_to be_success
-				expect(response).to have_http_status(302)
-			end
-		end
-	end
+  describe "GET #edit" do
+    context "User is logged in and tries to edit profile" do
+      before do
+        sign_in user_two
+      end
 
-	
-	describe "GET #show" do
-		context "User viewing other user profiles" do
-			before do
-				sign_in user_two
-			end
+      it "should redirect to the user's edit page" do
+        get :edit, id: user_two.id
+        expect(response).to have_http_status(200)
+      end
 
-			it "should block user and redirect to root path" do
-				get :show, id: user.id
-				expect(response).to redirect_to("/")
-			end
-		end
-	end
+      it "should block other users from editing pages" do
+        get :edit, id: user.id
+        expect(response).not_to be_success
+        expect(response).to have_http_status(302)
+      end
+    end
+  end
 end
--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,28 +1,19 @@
 FactoryGirl.define do
-sequence(:email) { |n| 'user#{n}@email.com' }
-	factory :user do
-		email 
-		password 'test123'
-		first_name 'John'
-		last_name 'Smith'
-		admin false
-	end
+  sequence(:email) { |n| "user#{n}@example.com" }
 
-	factory :admin, class: User do
-		email 
-		password 'test123'
-		admin true
-		first_name 'Admin'
-		last_name 'User'
+  factory :user do
+    email
+    password 'test123'
+    first_name 'John'
+    last_name 'Smith'
+    admin false    
+  end
 
-	end
-
-	factory :user_two, class: User do 
-		email
-		password 'test123'
-		first_name 'Jenny'
-		last_name 'Smith'
-		admin false
-	end
-
+  factory :admin, class: User do
+    email
+    password 'test123'
+    admin true
+    first_name "Admin"
+    last_name "User"
+  end
 end
