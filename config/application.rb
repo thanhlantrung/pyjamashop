553c64a66e99282993218f5e41f2a294f41546b5
Thanh Trung <thanhlan.trung@gmail.com>
2016-08-08 12:58:12 +0800

    added redis.rb

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require_relative 'boot'
 
 require 'rails/all'
 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -11,7 +12,9 @@ module PyjamaShop
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+ 	config.cache_store = :redis_store, 'redis://localhost:6379/0/cache'
   end
 end
 
 ActiveSupport.halt_callback_chains_on_return_false = false
+
