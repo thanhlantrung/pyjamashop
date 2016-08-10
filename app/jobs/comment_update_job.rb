4243c906b877274c7fb7878ac283c7125e044bb6
Thanh Trung <thanhlan.trung@gmail.com>
2016-08-10 15:10:06 +0800

    6.14 activejob

---
+++ app/jobs/comment_update_job.rb
@@ -0,0 +1,15 @@
+class CommentUpdateJob < ApplicationJob
+  queue_as :default
+
+  def perform(comment, current_user)
+    # Do something later
+    ProductChannel.broadcast_to comment.product.id, comment: render_comment(comment, current_user)
+  end
+
+
+private 
+
+	def render_comment(comment, current_user)
+		CommentsController.render(partial: 'comment', locals: {comment: comment, current_user: current_user })
+	end
+end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,5 +5,6 @@ class Comment < ActiveRecord::Base
   	validates :user, presence: true
   	validates :product, presence: true
   	validates :rating, numericality: { only_integer: true }
+  after_create_commit { CommentUpdateJob.perform_later(self, @user) }
 end
 
